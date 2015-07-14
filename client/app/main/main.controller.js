0fe6c8d25e9fea1a1f541b4ce21d2969569f8644
Justin <zertukis@gmail.com>
2015-07-13 21:58:05 -0500

    started moving infinitescroll to directive

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -16,7 +16,6 @@ angular.module('randomImgurApp')
 			}
 			return generatedString;
 		};
-
 		$scope.loadImages = function() {
 			var count = 0;
 			for (var i = 0; i < 52; i++) {
@@ -43,17 +42,4 @@ angular.module('randomImgurApp')
 		}
 
 		$scope.loadImages();
-
-		angular.element($window).bind("scroll", function() {
-		    var windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
-		    var body = document.body, html = document.documentElement;
-		    var docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
-		    var windowBottom = windowHeight + window.pageYOffset;
-		    if (windowBottom >= docHeight) {
-		       $scope.loadImages();
-		    }
-		});
-
-
-
 	});
